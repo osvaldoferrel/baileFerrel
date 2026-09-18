5bf4c4a02a356527cc70b118966cc1ac31d3e340
osvaldoferrel <1911942a@umich.mx>
2026-09-17 23:15:11 -0600

    Update dificil.py

--- src/dificil.py
+++ src/dificil.py
@@ -1,27 +1,20 @@
 from explorador import Laberinto
 
-
 mapa = [
-
     "#############",
     "#E....#....S#",
     "#.....#.....#",
     "#..K..D.....#",
     "#...........#",
     "#############"
-
 ]
 
 llaves = {
-
     (3, 3): "K1"
-
 }
 
 puertas = {
-
     (3, 6): "K1"
-
 }
 
 laberinto = Laberinto(
@@ -31,47 +24,33 @@ laberinto = Laberinto(
 )
 
 pruebas = [
-
     ("BFS", "bfs", False),
     ("DFS", "dfs", False),
     ("A* h=0", "a*", False),
     ("A* Manhattan", "a*", True)
-
 ]
 
 for nombre, estrategia, heuristica in pruebas:
-
     solucion, explorados = laberinto.resolver(
         estrategia,
         heuristica
     )
-
     print()
     print(nombre)
-
     if solucion:
-
         camino = solucion.getPath()
-
         print("Movimientos:",
               len(camino) - 1)
-
         print("Estados explorados:",
               explorados)
-
         print("Ruta:")
-
         for estado, operacion, profundidad in camino:
-
             posicion, llaves = estado
-
             print(
                 profundidad,
                 operacion,
                 posicion,
                 list(llaves)
             )
-
     else:
-
         print("No hay solución")
