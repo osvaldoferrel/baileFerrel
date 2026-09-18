73518290b3f69c62f3e6487034a36c710bc11d99
osvaldoferrel <1911942a@umich.mx>
2026-09-17 23:14:18 -0600

    Revise labyrinth map and key/door configurations
    Updated maze layout and key/door mappings for the labyrinth.

--- src/ultradificil.py
+++ src/ultradificil.py
@@ -2,36 +2,34 @@ from explorador import Laberinto
 
 mapa = [
     "###############",
-    "#E....#.......#",
-    "#.###.#.#####.#",
-    "#...#.#.....#.#",
-    "###.#.#####.#.#",
-    "#...#...K1#...#",
-    "#.#######.###.#",
-    "#...K2..D2....#",
-    "#.###.#######.#",
-    "#...#...K3....#",
-    "#.#.#####.###.#",
-    "#.#...D3...#..#",
-    "#.#####.##.#D4#",
-    "#K4....D1....S#",
+    "#E............#",
+    "#####.#######.#",
+    "#############.#",
+    "#.............#",
+    "#.#######.#####",
+    "#.#############",
+    "#.............#",
+    "#####.#######.#",
+    "#############.#",
+    "#.............#",
+    "#.#######.#####",
+    "#.#############",
+    "#............S#",
     "###############"
 ]
 
 llaves = {
-    (5, 8): "K1",
-    (7, 4): "K2",
-    (9, 8): "K3",
-    (13, 1): "K4"
-
+    (2, 5): "K1",
+    (5, 9): "K2",
+    (8, 5): "K3",
+    (11, 9): "K4"
 }
 
 puertas = {
-    (7, 8): "K2",
-    (11, 6): "K3",
-    (13, 7): "K4",
-    (13, 12): "K1"
-
+    (2, 13): "K1",
+    (5, 1): "K2",
+    (8, 13): "K3",
+    (11, 1): "K4"
 }
 
 laberinto = Laberinto(
@@ -45,37 +43,22 @@ pruebas = [
     ("DFS", "dfs", False),
     ("A* h=0", "a*", False),
     ("A* Manhattan", "a*", True)
-
 ]
 
-print("LABERINTO ULTRADIFICIL")
-print("Tamaño: 15 x 15")
-print("Llaves:", len(llaves))
-print("Puertas:", len(puertas))
-
-for nombre, estrategia, usar_heuristica in pruebas:
-    print()
-    print("-------------------------")
-    print(nombre)
-    print("-------------------------")
+for nombre, estrategia, heuristica in pruebas:
     solucion, explorados = laberinto.resolver(
         estrategia,
-        usar_heuristica
+        heuristica
     )
+    print()
+    print(nombre)
+
     if solucion:
         camino = solucion.getPath()
         print("Movimientos:",
               len(camino) - 1)
         print("Estados explorados:",
               explorados)
-        print("Camino:")
-        for estado, movimiento, profundidad in camino:
-            posicion, llaves_actuales = estado
-            print(
-                profundidad,
-                movimiento,
-                posicion,
-                list(llaves_actuales)
-            )
     else:
-        print("No se encontró una solución.")
+
+        print("No hay solución")
