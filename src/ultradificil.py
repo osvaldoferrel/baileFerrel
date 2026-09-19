e0f09fca4608237862129b7cbc96e74ad1c9824f
osvaldoferrel <1911942a@umich.mx>
2026-09-19 00:11:09 -0600

    Update ultradificil.py

--- src/ultradificil.py
+++ src/ultradificil.py
@@ -31,34 +31,3 @@ puertas = {
     (8, 13): "K3",
     (11, 1): "K4"
 }
-
-laberinto = Laberinto(
-    mapa,
-    llaves,
-    puertas
-)
-
-pruebas = [
-    ("BFS", "bfs", False),
-    ("DFS", "dfs", False),
-    ("A* h=0", "a*", False),
-    ("A* Manhattan", "a*", True)
-]
-
-for nombre, estrategia, heuristica in pruebas:
-    solucion, explorados = laberinto.resolver(
-        estrategia,
-        heuristica
-    )
-    print()
-    print(nombre)
-
-    if solucion:
-        camino = solucion.getPath()
-        print("Movimientos:",
-              len(camino) - 1)
-        print("Estados explorados:",
-              explorados)
-    else:
-
-        print("No hay solución")
